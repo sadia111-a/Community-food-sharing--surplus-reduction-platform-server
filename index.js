c4bd14c860b8ca7d12faf5cc2b6a2c50c3c9e5fa
sadia11-a <sadiasorna96@gmail.com>
2023-11-06 00:31:50 +0600

    find all my api data from foodCollection and use the data for fetch

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodCollection = client.db("foodSharing").collection("foods");
+
+    app.get("/foods", async (req, res) => {
+      const cursor = foodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -33,7 +41,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
